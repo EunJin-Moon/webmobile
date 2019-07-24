40d32f6a178c0f0b8c64b2aa82ee09a78064eb2f
jin__y <jinneey05@gmail.com>
2019-07-24 17:51:03 +0900

    postwrite첫번째단계
    Former-commit-id: 5e4db40eaffe03a1d887e4dfba8218fa14e82491

--- src/components/PostWrite.vue
+++ src/components/PostWrite.vue
@@ -1,18 +1,3 @@
-<template>
-<div>
-  여기다여기<br>
-  여기다여기<br>
-  여기다여기<br>
-  여기다여기<br>여기다여기<br>
-  여기다여기<br>여기다여기<br>
-  여기다여기<br>여기다여기<br>
-  여기다여기<br>
-</div>
-</template>
-
-<script>
-</script>
-
-<style>
-
-</style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:d641e391d2896a5c207aaa3bbfda53eb159456f5b5998d0f49cd60c7923b3ae5
+size 1397
