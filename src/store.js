b087666da513d795ab0037078976a1362efd6ce3
godqhr5627 <godqhr5627@naver.com>
2019-07-24 16:52:16 +0900

    vuex 방식으로 저장방법 변경
    Former-commit-id: b7dde6fd9cdad864918b665c0018d08ba6323e14

--- src/store.js
+++ src/store.js
@@ -1,11 +1,3 @@
-import Vue from "vue"
-import Vuex from "vuex"
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-    accessToken: "",
-    user: ""
-  }
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:64ba3ea2e0e30e46e8f08794a59b7f59a05065533a43078c6e6c79b6bd4afdea
+size 665
