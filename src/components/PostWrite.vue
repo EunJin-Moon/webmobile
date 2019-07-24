632f7f5c0fa2172a79522a7ec05dac45a522b8c5
jin__y <jinneey05@gmail.com>
2019-07-24 17:51:03 +0900

    postwrite첫번째단계
    Former-commit-id: 469dbe7a8b02f3e1973aa8cfe7408cd494da89c3

--- src/components/PostWrite.vue
+++ src/components/PostWrite.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e12e93845ea5d291194a1fe78781779d51eaf31df25e041c809574c3919a920c
-size 286
+oid sha256:d641e391d2896a5c207aaa3bbfda53eb159456f5b5998d0f49cd60c7923b3ae5
+size 1397
