07ecf7991dc47b4d229eea14a537f77ced6c65ba
godqhr5627 <godqhr5627@naver.com>
2019-07-25 14:14:23 +0900

    페이스북, 구글 아이콘 변경
    Former-commit-id: b23f03ff9162babe4d3fe5236810ef5b98b44623

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8b8871d5bc4924209dc3e401b7bbb4c475e43b9dd36bfee284c82e537f44d8
-size 2612
+oid sha256:b07d804fc0d6db972d78d86bbaef6e99dad750bfc62c482a453c897add1204c9
+size 2634
