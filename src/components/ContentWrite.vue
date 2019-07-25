2fd5876492926b842c17c279ba0e686217b387ce
jin__y <jinneey05@gmail.com>
2019-07-25 17:07:06 +0900

    포스트/폴리오글쓰기버튼Write하나로통합하기ver1
    Former-commit-id: 67b97bc6a5796f333909abfc4e2899e8f1e7876c

---
+++ src/components/ContentWrite.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:65a81821757e432c82dbd1a10753523e097920dac08413bdb179f3253bc94038
+size 2332
--- src/components/PostWrite.vue
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d641e391d2896a5c207aaa3bbfda53eb159456f5b5998d0f49cd60c7923b3ae5
-size 1397
