2065bba6a1f014c65c5790654cf5c70bf381b44b
godqhr5627 <godqhr5627@naver.com>
2019-07-26 17:26:49 +0900

    관리자 페이지 생성 및 멤버 데이터를 표로 구성
    Former-commit-id: 3141d76c31aa435823c792ec6a1a2a5b5074da1b

--- src/router.js
+++ src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dcb655b96f8ff016af436bb1135e0184ecd919e91db6bff776dfb185c4edd9a
-size 689
+oid sha256:ea8c3b696460f342be4c7f34d4a6f34bade2bf2da62c853829924d67f8c90104
+size 997
---
+++ src/views/AdminPage.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aacb297fa2280c89f629e9f868df4098815f9cdea806fe7c98074ed36f0af1fe
+size 1272
