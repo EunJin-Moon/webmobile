3d9849f017f2294b100e90e5510cc0840f5ab16a
Kimyong <kkskww@gmail.com>
2019-07-29 17:44:56 +0900

    vuetify버전 업
    Former-commit-id: d3fcc9aa768d17f251c05e7aa128d7004cf68279

--- src/router.js
+++ src/router.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:868a3044b0c5dd3abc36a5f3081d94650ccc439e0edbbccc723ece3c72c4b680
-size 1101
+oid sha256:fe4666ec49b21b355abd67f9cbbc721719c3b8619be2d3afe39b49b2e167ff36
+size 1268
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0221cb695c132550b5dba86abfed275690ab4c77b159bcf1f69716ab3d02cb72
-size 656
+oid sha256:67f4c85c9489b2a3d9303a98a32507bd7fab06cec511999d378ec9237fa24302
+size 586
