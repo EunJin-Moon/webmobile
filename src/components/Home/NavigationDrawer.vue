ff3500fa98c7bdbad7925ce36502bcfe07bb8241
phaethon5882 <phaethon5882@naver.com>
2019-07-29 16:58:14 +0900

    computed 값을 읽을 때 v-model 을 v-bind 로 바꿔 오류창 안 뜨게 함
    Former-commit-id: 4731fee8b8d903dc1be490fedc5391c7368c2067

--- src/components/Home/NavigationDrawer.vue
+++ src/components/Home/NavigationDrawer.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eca9a8a308c954ebd6049a2e71927c76cb18155db5b3491d08f5599ee927c7e
-size 2902
+oid sha256:3c29bbb51e52b0b472140eeb187f5c74b471434ca6babd46e863fd6b80006b6f
+size 2901
