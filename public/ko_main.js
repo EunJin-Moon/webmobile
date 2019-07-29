b160946251cf8a3cff7e3bb273b14429fdca4f80
phaethon5882 <phaethon5882@naver.com>
2019-07-29 12:07:04 +0900

    복구했지만, 그래프는 안 예쁠 것이다.

---
+++ public/ko_main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a57bb1debb068f336b21a9bc242baf39b9ca7eac3de2f61e0c2d2e9f48c6525
+size 5503
---
+++ src/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9a402e51c0584bb049d774d2d445eb96ad07642eef92a102989acb9305f526e
+size 401
