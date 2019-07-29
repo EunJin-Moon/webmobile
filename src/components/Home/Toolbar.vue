7f207e4fe510bdccb4cc8834a899972fc78dc5bb
phaethon5882 <phaethon5882@naver.com>
2019-07-29 14:55:31 +0900

    툴바 링크 수정
    Former-commit-id: 0baffe461ab8ac3b214a46bb12b96e993b878123

--- src/components/Home/Toolbar.vue
+++ src/components/Home/Toolbar.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70134c7ab82de8a58666aeeba72b5d6845bfd159f39635305630c0195f4d42dc
-size 791
+oid sha256:bd71a4c7aba4d74917d66aa1e0be61ec1a9a484dfb00a9ff3e4227fc86409f13
+size 818
