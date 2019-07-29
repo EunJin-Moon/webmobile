6a4b9de7275500431ad25c9cb7615fe8f9cad2cc
godqhr5627 <godqhr5627@naver.com>
2019-07-29 17:13:36 +0900

    sign in 간략화
    Former-commit-id: 3e92cbdd841ac72a536c0a273793d0d018ccd290

--- src/components/Member/SignIn.vue
+++ src/components/Member/SignIn.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c672ad01db53e9a587eb6f729a61f0a43b0e708cf50e5c33dae6f8ecdc68f5a3
-size 2462
+oid sha256:bbd368f6701d8d8208db07ff6345cd7581c3df24be5ddb952e4cb28954ba8241
+size 2514
