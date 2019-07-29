69065921222a28af83bda90b80cfdbc050afca6b
phaethon5882 <phaethon5882@naver.com>
2019-07-29 16:45:46 +0900

    번역기 동적 삽입방식으로 변경
    Former-commit-id: 6fa8b24eaf9054ec4ad1663a39708a9aa079822e

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a5f08b74bb551acd39e71f107ff3b81167192973d6ea4d5476a1288222fb19b
-size 1288
+oid sha256:785fd2e25dfbf607c21a151aa08c10ccb76f92d8fd275e9a0571c9c5ebd8d584
+size 1363
--- src/components/Home/NavigationDrawer.vue
+++ src/components/Home/NavigationDrawer.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94d256e9a4f7b57dbe2a44e46b1ad263a7bf4282a5cf31f36779567b3ec2594f
-size 2245
+oid sha256:9eca9a8a308c954ebd6049a2e71927c76cb18155db5b3491d08f5599ee927c7e
+size 2902
