a0d050ddef773b558d844947682230c8396170d0
phaethon5882 <phaethon5882@naver.com>
2019-07-30 17:57:25 +0900

    드로워열릴시 사이즈 엉망진창 되던 버그 수정
    Former-commit-id: d9867294f4545422bf26ea9489e25bb1e1676df0

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8540453cf7f4374ba96d3dfe43cdf80fd87bca54fb222d7bff66295babd72014
-size 1463
+oid sha256:ea85860e09cd0cca31f117836381c49cf368fbe07e477baec0c6a6d0c0fa2203
+size 1102
