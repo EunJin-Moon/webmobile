e3febad9ca711524d28659905bd7e118fd40a6c2
godqhr5627 <godqhr5627@naver.com>
2019-07-30 16:23:53 +0900

    store 에 멤버객체를 담을 변수 선언
    Former-commit-id: 2769aa5e4065d8f1f6d1323e3e9cfcf987eaf938

--- src/store.js
+++ src/store.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca1b97884a3191fa1b06daa47a894cd0383a8e6b61f3abe33eed471f2aae2cec
-size 1192
+oid sha256:c6949e069148b9bd8dc27884293f4b60cec3d52941515559758b1ffc83ffdfa6
+size 1275
