b41e8c82339c5cdc7644ce2f4c307947a3414288
phaethon5882 <phaethon5882@naver.com>
2019-07-31 14:49:28 +0900

    링크 이동 시에 드로워가 클릭 안 되던 버그 수정
    Former-commit-id: 21c11ff4d90ddb244448a7b8f30745585c26840b

--- src/views/GitFlowPage.vue
+++ src/views/GitFlowPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:117c958baf8fe6a5a3d43a77661d25b0996d207e9267142fe805a182a16f13cf
-size 4031
+oid sha256:d1f0384d1db74e1bcd722593ed5b8fe55e80128c9c6166adb0223b146145303a
+size 3973
