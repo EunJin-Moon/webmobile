5f9e6452c531e4c6569273240d9367fcc1461350
jin__y <jinneey05@gmail.com>
2019-07-31 09:45:23 +0900

    v포토폴리오에도 글쓰기버튼 완료
    Former-commit-id: e35b78dec8fe7029ebdfcee3cd1806af43976f7e

--- src/components/Document/PortfolioList.vue
+++ src/components/Document/PortfolioList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6ff9323d13527f954e719c7579337561361b575348eb2380865f23cecfb5f71
-size 3703
+oid sha256:87c21d787c6970f45ca0c9ca45e89e8cb5ad0b406cf67a7883d75cec221f47e3
+size 3731
--- src/components/Document/PostList.vue
+++ src/components/Document/PostList.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57a8d69f195147f99c33bbc7e5f91b01b1cf398d40650fa784acd69ee0e1a4c2
-size 1473
+oid sha256:97631fb554d463d29ef0af8a5dbf37440df2f662965bff72ffe0ef3ef932ee79
+size 1462
