663d1bfc3daeb3fc5ffdab4b340052c95d9d5fb1
godqhr5627 <godqhr5627@naver.com>
2019-07-31 17:07:03 +0900

    이메일 로그인시 등장하는 사진을 기본 사이트로고 사진으로 변경
    Former-commit-id: bd92619b87d991e9faea21fc1a38d20b0131ca6e

--- src/components/Home/NavigationDrawer.vue
+++ src/components/Home/NavigationDrawer.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03a44b50bbf68e6e7eb6cbac647b8491ae25240cd4907294f61fccb614961c88
-size 4191
+oid sha256:580ad21956779b8e71e6d7ddf595de2dbba797cbf41f27c33544750fd3bbbf4f
+size 4288
