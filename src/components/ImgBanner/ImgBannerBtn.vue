769b41de7b912b3c0c650b2ae26d408bdc49ee27
BackYunki <byg9543@gmail.com>
2019-08-02 01:22:26 +0900

    이미지배너에 버튼 배치 및 그리드 시스템 적용
    Former-commit-id: c7f10978fb7561e5e7078f44843df0c938574d2f

--- src/components/ImgBanner/ImgBannerBtn.vue
+++ src/components/ImgBanner/ImgBannerBtn.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b604d0acef071d78ae5e6ee8362578d477fd8917f1a64a23da84093b21f8a12
-size 3731
+oid sha256:ec828b3d443f07ec7b8f5d7f4906a38bad6170ab0b52b9558bc023b11e9f0074
+size 3484
