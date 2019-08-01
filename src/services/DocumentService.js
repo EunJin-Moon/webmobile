ff7d4258eb4d9c8c901e324d578ca9612c767f18
Kimyong <kkskww@gmail.com>
2019-08-01 17:55:29 +0900

    post/portfolio 글쓰기 버튼 조건 수정/ post 댓글 기능 추가(수정필요)
    Former-commit-id: 43f7e044d2ee1b1bc09036ed9ae96a10f5822ef0

--- src/services/DocumentService.js
+++ src/services/DocumentService.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4f3666cc2019a6c51fed7b30f979459a715df414699f5f308ac176675afbfbc
-size 2689
+oid sha256:955bd65d3b26e9506e37c5735608edec70542d526cb5003254444567e5ecbe1d
+size 3874
