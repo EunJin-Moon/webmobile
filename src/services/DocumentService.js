7dbecb70977574769a5185ab50159cebc25fba39
jin__y <jinneey05@gmail.com>
2019-08-01 17:28:52 +0900

    device token ver1
    Former-commit-id: e30ab9af1f196227d0c4d1890761a71f45622408

--- src/services/DocumentService.js
+++ src/services/DocumentService.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4f3666cc2019a6c51fed7b30f979459a715df414699f5f308ac176675afbfbc
-size 2689
+oid sha256:3247233ee91d19c1ca23784780dea30c5ab86afe073f05845512574b821a1cc9
+size 2661
