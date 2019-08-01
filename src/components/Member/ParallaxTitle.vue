f2365c544a7de6e30d3781652d4cffa80a04d1bc
BackYunki <byg9543@gmail.com>
2019-08-01 22:53:56 +0900

    add grid system and title component to the admin page.
    Former-commit-id: 5225132b1ccb8ee80dda5c4efa2aeb1b3aceaf99

---
+++ src/components/Member/ParallaxTitle.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76866228dc51604cb0c1618eee0acc1ce306f9e4a3a44fd92640b49995211241
+size 841
--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9f67f36c4d24c78b91a625d981c2be280f935fc383dc6f4f26ef5e062d6288d
-size 3348
+oid sha256:0133d3587583325825bd3ce03fbacb61ce8bb9691a2b2821728607788234c907
+size 4103
