598e540946caad6945d518a741358f4102124107
godqhr5627 <godqhr5627@naver.com>
2019-08-01 16:43:13 +0900

    회원관련 UI를 좀 더 가독성 있게 CSS 조정
    Former-commit-id: 37c72e34c3bf09e5f2d1778a1f66b2bbe6ebadfa

--- src/components/Member/UserInfoMenu.vue
+++ src/components/Member/UserInfoMenu.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36083e8cceca08bf1a6589ab671063c268893233c02bba3627200457ed51036c
-size 1843
+oid sha256:1d554574dc2d79b0790d15e8a1e875ac907ee5ddcf14bce5f043cbadeb1f00c9
+size 1903
