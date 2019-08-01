53ff48a7abd9c55e259c366870ed75d517f74ba3
jin__y <jinneey05@gmail.com>
2019-08-01 17:28:52 +0900

    device token ver1
    Former-commit-id: 8275e91c011adb2d451e95277e2c9f76b8ec274e

--- src/services/MemberService.js
+++ src/services/MemberService.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41c9e38fdfeef94f404ad87b344bf855c1bd5961baac90dc2c74a8c2000a5e51
-size 3814
+oid sha256:9cf5c4de83a9f19e43eae4e98368f4fe46f1c2e545619af19ddf507c4f1f15ad
+size 4540
