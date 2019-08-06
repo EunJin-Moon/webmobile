3aad20cce31e7f4274fb4d080e1d9540cc0fee91
Kimyong <kkskww@gmail.com>
2019-08-06 15:45:50 +0900

    post 대댓글 기능 가능(css 수정 필요)
    Former-commit-id: 4d79cd028d457f2385abe291bbcaf7baac4215f8

--- src/components/Document/Comment.vue
+++ src/components/Document/Comment.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7af4ae095ea73da41593ea0b2a5608cca24c6c8a34261e44d1a1cfa55e190ca8
-size 2369
+oid sha256:d0a99262fc437e75243bac3696590ab42916ca6975ef98d406279c72a126ef9f
+size 2865
