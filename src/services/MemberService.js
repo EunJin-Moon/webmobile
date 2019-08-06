396f95dfc0066d3fa35efd85c899e755fc7916e2
godqhr5627 <godqhr5627@naver.com>
2019-08-06 15:35:14 +0900

    데이터베이스 구조 개조화 // before after 함수 따로 구현
    Former-commit-id: 498aedea748bfafad9e4071e9dc21baaab98853d

--- src/services/MemberService.js
+++ src/services/MemberService.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:683a339a6d583deedc24a2b20cfeecfbfd73edfc4f8e343bc74d1762fe2fb923
-size 4962
+oid sha256:4d3e7cc90ac1d977a8919fbc0c7d9bb51e50c8fc22eef517343a218df718f220
+size 5651
