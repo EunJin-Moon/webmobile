05808832224841691443ce35297f7e0034bae541
jin__y <jinneey05@gmail.com>
2019-08-06 15:34:48 +0900

    포스트올릴때드디어나한테알림이옴. 토큰값다른사람에게받아야만알림확인가능
    Former-commit-id: 21ec80eee642186cf2cf2751dec55be1444b2e0a

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10e899b378042d20201e475cacdbe7ad3914a7d7eea8300e89823e5e6d0e84d3
-size 675
+oid sha256:6261264960c206372974a5b607123ac4b1396bc56cc6c4c508e0839bab254d3f
+size 734
