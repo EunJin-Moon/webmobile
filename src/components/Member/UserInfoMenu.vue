fe562c65261fac7c6789de4b49445b3e91773abe
godqhr5627 <godqhr5627@naver.com>
2019-08-06 15:34:43 +0900

    로그인 및 회원가입을 했을 때 세션에 나의 권한을 올려놓고 로그아웃하면 지움
    Former-commit-id: 56712b9cc576075eb1103db6e150e74bebb9501f

--- src/components/Member/UserInfoMenu.vue
+++ src/components/Member/UserInfoMenu.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d554574dc2d79b0790d15e8a1e875ac907ee5ddcf14bce5f043cbadeb1f00c9
-size 1903
+oid sha256:60ca63074e579129a172bca2e84193f25d06f9d356dc61ad6320ede30af503c6
+size 1931
