6f62abad9dec81d7d4ba6a0293023c182df0685d
godqhr5627 <godqhr5627@naver.com>
2019-08-07 16:33:36 +0900

    데이터베이스에 필요한 필드값 추가 입력 구현
    Former-commit-id: ccfe6a7670f323004f2a11acc19622653a55c3ee

--- src/components/Member/SignUp.vue
+++ src/components/Member/SignUp.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:787b4a484111af12e09230c65ef6acd7b3bdea57bc0f288590b67148e9b8fe82
-size 2379
+oid sha256:d123df8722af495a333e5b0274ee6106019177d09246f7f7cd19282fa64b7f7c
+size 2273
