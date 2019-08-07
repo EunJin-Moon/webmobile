69b0ddf4051ea99def83401137fe617ae575a903
Kimyong <kkskww@gmail.com>
2019-08-07 15:20:35 +0900

    post댓글 기능 가능
    Former-commit-id: e4c42271fe7947e6962ae50cd5ee6e316bd9c95a

--- src/components/Document/Comment.vue
+++ src/components/Document/Comment.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0a99262fc437e75243bac3696590ab42916ca6975ef98d406279c72a126ef9f
-size 2865
+oid sha256:9f921f956121f45d6373ff33f209787eb5ab3ec444eec46a3187533b24af24e9
+size 3064
