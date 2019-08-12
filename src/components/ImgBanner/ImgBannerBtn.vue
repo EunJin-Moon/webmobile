71451bfbeb45cedc449355df3b5c78241def01a4
Kimyong <kkskww@gmail.com>
2019-08-12 17:35:49 +0900

    img선택 css수정중
    Former-commit-id: 5246e738a33f57f576b5d906f60b030dc42b9671

--- src/components/ImgBanner/ImgBannerBtn.vue
+++ src/components/ImgBanner/ImgBannerBtn.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07cc2f87b397d049871b3681ded8daf7cafb793da262f39e5cfb4eb7c68fb888
-size 3728
+oid sha256:bb94715561ef1907b8ace5d2e258c242502dae2ca4b1f1fa6d250c7d2f12effb
+size 4153
