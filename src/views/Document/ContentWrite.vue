0c41f66ce30c55a480ef77755c210c75f78304c7
Kimyong <kkskww@gmail.com>
2019-08-13 17:02:08 +0900

    contentWrite v-btn flat -> text로 변경
    Former-commit-id: cb7b446640597378c04f81142f25583184f9670d

--- src/views/Document/ContentWrite.vue
+++ src/views/Document/ContentWrite.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37eb1e5a54df12c0712f35f1dc85c1af6ca1c5420f5d5ab271c71324827fea91
+oid sha256:4a79de5279345b3bd6e76f4c980ab9d2b0995050b0477c1bcc26ade5c3873264
 size 3928
