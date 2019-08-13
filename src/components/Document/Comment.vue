68753a4ad1e8cbe69eb96edea84851d70073ca88
jin__y <jinneey05@gmail.com>
2019-08-13 16:30:02 +0900

    comment 제한수알림과 30자로제한
    Former-commit-id: b36fdaf1a1863f4ad0afa04e443eadf6e61e5967

--- src/components/Document/Comment.vue
+++ src/components/Document/Comment.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:208a36c3bdab64cd29cda8f5398821ea78f61f37941c7307ac3b32ed95fa8f64
-size 4578
+oid sha256:35b96e06061c4dda1f565630c2b8fb13714020987459001cb910a4dacdb73a14
+size 4785
